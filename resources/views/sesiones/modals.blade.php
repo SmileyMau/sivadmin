e65d1cf35da97432b9b41450709b953b1caa5a63
smileymau <loki_cad@hotmail.com>
2026-06-04 11:43:04 -0600

    arreglos

--- resources/views/sesiones/modals.blade.php
+++ resources/views/sesiones/modals.blade.php
@@ -124,8 +124,13 @@
                             </select>
                         </div>
                     </div>
-                    <div class="form-group" style="display: none;">
-                        <input type="hidden" id="input_tipo_archivo" name="tipo_archivo" required  readonly onmousedown="return false;"  value="ACTA">
+                    <div class="mt-3">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><b>Archivo</b></span>
+                            </div>
+                            <input type="file" class="form-control" accept="application/pdf" name="archivo" id="" placeholder="Orden del dia" required>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
@@ -177,72 +182,68 @@
 
 <script>
 
-  var c=0;
-  function newInput()
-  {
-    c+=1;
-    document.getElementById("form_det").insertAdjacentHTML('beforeend',' <div class="" id="div_'+c+'"defer><div class="row g-2 mb-2" ><div class="col-12"><div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="nav-icon fas fa-copy"></i></span></div><select name="tipo'+c+'" id="select_tipo_voto'+c+'" data-count="'+c+'"  class="form-control" required onchange="mostrar_imputs(this)"><option value="0" data-tipo="0">Seleccionar tipo...</option>@foreach ($tipo_asuntos as $tipo_asunto)<option value="{{$tipo_asunto->id}}" data-tipo="{{$tipo_asunto->id}}">{{$tipo_asunto->descripcion}}</option>@endforeach</select><select name="asunto'+c+'" id="select_asunto' + c + '"  class="form-control" hidden ><option value="">Seleccionar asunto...</option></select><input hidden type="text" class="form-control" name="titulo' + c + '" id="titulo'+c+'" placeholder="Titulo."  ><input hidden style="width: 30%;" type="text" class="form-control" name="descripcion'+c+'" id="descripcion'+c+'" placeholder="Descripción del dictamen." ></div></div></div></div>');
+    var c=0;
+    function newInput()
+    {
+        c+=1;
+        document.getElementById("form_det").insertAdjacentHTML('beforeend',' <div class="" id="div_'+c+'"defer><div class="row g-2 mb-2" ><div class="col-12"><div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="nav-icon fas fa-copy"></i></span></div><select name="tipo'+c+'" id="select_tipo_voto'+c+'" data-count="'+c+'"  class="form-control" required onchange="mostrar_imputs(this)"><option value="0" data-tipo="0">Seleccionar tipo...</option>@foreach ($tipo_asuntos as $tipo_asunto)<option value="{{$tipo_asunto->id}}" data-tipo="{{$tipo_asunto->id}}">{{$tipo_asunto->descripcion}}</option>@endforeach</select><select name="asunto'+c+'" id="select_asunto' + c + '"  class="form-control" hidden ><option value="">Seleccionar asunto...</option></select><input hidden type="text" class="form-control" name="titulo' + c + '" id="titulo'+c+'" placeholder="Titulo."  ><input hidden style="width: 30%;" type="text" class="form-control" name="descripcion'+c+'" id="descripcion'+c+'" placeholder="Descripción del dictamen." ></div></div></div></div>');
 
-    $("#idcount").val(c);
-    $("#btn_deleteinput").show();
+        $("#idcount").val(c);
+        $("#btn_deleteinput").show();
 
-  }
-
-  function deleteInput()
-  {
-    if (c != 0) {
-      
-      div = "#div_" + c;
-      br = "#br_" + c;
-      console.log(div);
-      $(div).remove();
-      $(br).remove();
-      c = c - 1;
-      $("#idcount").val(c);
     }
-    if(c == 0){
-      $("#btn_deleteinput").hide();
-    }
-  }
 
-  function tipo_archivo(option) {
-    var data_tipo = $('#' + option.id).attr('data-tipo_archivo');
-    var data_texto = $('#' + option.id).attr('data-texto');
-    console.log(data_texto);
-    $('#input_tipo_archivo').val(data_tipo);
-    $("#h5_titulo").text(data_texto);
-    
-
-  }
-
-  function mostrar_imputs(option){
-    var tipo = $('#' + option.id + ' option:selected').attr('data-tipo');
-    var count = $('#'+option.id).attr('data-count');
-    console.log('select_asunto' + count);
-    if (tipo == 5 || tipo == 7 || tipo == 8 || tipo == 9) {
-      $('#titulo' + count).removeAttr('hidden');
-      $('#descripcion' + count).removeAttr('hidden');
-      $('#select_asunto' + count).attr('hidden',true);
-    }else{
-      var id = $('#select_partida option:selected').attr('value');
-      $('#select_asunto' + count).empty();
-      
-      //console.log(id);
-      document.getElementById('select_asunto' + count).insertAdjacentHTML('beforeend',' <option selected="selected">Seleccionar asunto...</option>');
-      
-      @foreach($asuntos as $asunto)
-        if ({{$asunto->id_tipo}} == tipo) {
-          document.getElementById('select_asunto' + count).insertAdjacentHTML('beforeend','  <option value="{{$asunto->id}}" >'+'{{$asunto->titulo}}'+'</option>');
+    function deleteInput()
+    {
+        if (c != 0) {
+        
+        div = "#div_" + c;
+        br = "#br_" + c;
+        console.log(div);
+        $(div).remove();
+        $(br).remove();
+        c = c - 1;
+        $("#idcount").val(c);
         }
-      @endforeach
-
-      $('#titulo' + count).attr('hidden',true);
-      $('#descripcion' + count).attr('hidden',true);
-      $('#select_asunto' + count).removeAttr('hidden');
-
+        if(c == 0){
+        $("#btn_deleteinput").hide();
+        }
     }
-  }
+
+    function tipo_archivo(option) {
+        var data_tipo = $('#' + option.id).attr('data-tipo_archivo');
+        var data_texto = $('#' + option.id).attr('data-texto');
+        console.log(data_texto);
+        $('#input_tipo_archivo').val(data_tipo);
+        $("#h5_titulo").text(data_texto);
+    }
 
 
+    function mostrar_imputs(option){
+        var tipo = $('#' + option.id + ' option:selected').attr('data-tipo');
+        var count = $('#'+option.id).attr('data-count');
+        console.log('select_asunto' + count);
+        if (tipo == 5 || tipo == 7 || tipo == 8 || tipo == 9) {
+        $('#titulo' + count).removeAttr('hidden');
+        $('#descripcion' + count).removeAttr('hidden');
+        $('#select_asunto' + count).attr('hidden',true);
+        }else{
+        var id = $('#select_partida option:selected').attr('value');
+        $('#select_asunto' + count).empty();
+        
+        //console.log(id);
+        document.getElementById('select_asunto' + count).insertAdjacentHTML('beforeend',' <option selected="selected">Seleccionar asunto...</option>');
+        
+        @foreach($asuntos as $asunto)
+            if ({{$asunto->id_tipo}} == tipo) {
+            document.getElementById('select_asunto' + count).insertAdjacentHTML('beforeend','  <option value="{{$asunto->id}}" >'+'{{$asunto->titulo}}'+'</option>');
+            }
+        @endforeach
 
+        $('#titulo' + count).attr('hidden',true);
+        $('#descripcion' + count).attr('hidden',true);
+        $('#select_asunto' + count).removeAttr('hidden');
+
+        }
+    }
 </script>
